d1c950700fd176951068018d5dffd569203f6af7
gabesouto <soutogabriel04@gmail.com>
2023-04-03 19:35:55 -0300

    Req 6, falta os testes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import RecipePage from './pages/RecipePage';
 
 function App() {
   return (
     <div>
-      <Login />
+      <main>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/meals" component={ RecipePage } />
+        </Switch>
+      </main>
     </div>
   );
 }
