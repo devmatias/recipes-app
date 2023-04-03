6cb2cc02a0c0c1d2b76ee175213f278a8d3bb298
Thiago <xfatalgames@gmail.com>
2023-04-03 17:11:22 -0300

    parcialmente feito

---
+++ src/Login.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function Login() {
+    return (
+    <section>
+      <form>
+        <h1>Login</h1>
+        <section>
+          <input
+            placeholder="Email"
+            type="email"
+            data-testid="email-input"
+            />
+          <input
+            placeholder="Password"
+            type="password"
+            data-testid="password-input"
+          />
+        </section>
+        <section>
+          <button
+            type="button"
+            data-testid="login-submit-btn"
+          >
+            Enter
+          </button>
+        </section>
+      </form>
+    </section>
+  );
+}
+
+
+export default Login;
