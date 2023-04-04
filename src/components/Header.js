c1455e8e48d36a4c8bf3795de6c38008cc5c6172
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2023-04-04 17:24:28 -0300

    Requisito 08: Criando funcao e buttons de redirecionamento

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,24 @@
+import { useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header() {
+  const history = useHistory();
+
+  const redirectPage = (path) => {
+    history.push(path);
+  };
   return (
     <header>
-      <img
-        src={ profileIcon }
-        alt="profile Icon"
-        data-testid="profile-top-btn"
-      />
+      <button
+        onClick={ () => redirectPage('/profile') }
+      >
+        <img
+          src={ profileIcon }
+          alt="profile Icon"
+          data-testid="profile-top-btn"
+        />
+      </button>
       <img
         src={ searchIcon }
         alt="search Icon"
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,25 @@
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import profileIcon from '../images/profileIcon.svg';
 
 function Profile() {
+  const history = useHistory();
+
+  const redirectPage = (path) => {
+    history.push(path);
+  };
   return (
     <header>
       <h1 data-testid="page-title">Profile</h1>
-      <img
-        src={ profileIcon }
-        alt="profile Icon"
-        data-testid="profile-top-btn"
-      />
+      <button
+        onClick={ () => redirectPage('/profile') }
+      >
+        <img
+          src={ profileIcon }
+          alt="profile Icon"
+          data-testid="profile-top-btn"
+        />
+      </button>
       <Footer />
     </header>
   );
