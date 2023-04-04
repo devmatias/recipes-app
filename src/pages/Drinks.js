002bcebfb404cc68232ab635972788e8dd89f0ed
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2023-04-04 15:08:08 -0300

    Requisito 7:Criando header na page Drinks, e na page RecipePage

---
+++ src/pages/Drinks.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function Drinks() {
+  return (
+    <header>
+      <h1 data-testid="page-title">Drinks</h1>
+      <Header />
+    </header>
+  );
+}
+
+export default Drinks;
--- src/pages/RecipePage.js
+++ src/pages/RecipePage.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function RecipePage() {
   return (
-    <div>RecipePage</div>
+    <header>
+      <h1 data-testid="page-title">Meals</h1>
+      <Header />
+    </header>
   );
 }
 
