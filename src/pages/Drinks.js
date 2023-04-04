3119e69931269db1d14cf192c594b0bb5d53d660
devmatias <devmatiascff@gmail.com>
2023-04-04 15:51:43 -0300

    Requsiito 17 completo

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Drinks() {
   return (
     <header>
       <Header />
       <h1 data-testid="page-title">Drinks</h1>
+      <Footer />
     </header>
   );
 }
--- src/pages/RecipePage.js
+++ src/pages/RecipePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function RecipePage() {
   return (
     <header>
       <Header />
       <h1 data-testid="page-title">Meals</h1>
+      <Footer />
     </header>
   );
 }
