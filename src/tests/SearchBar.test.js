58d5ed7b21569ee1aa533fe59b3925ce8494bdc5
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2023-04-07 15:11:25 -0300

    Iniciando testes searchBar

--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+// import userEvent from '@testing-library/user-event';
 import SearchBar from '../components/SearchBar';
-import renderWithRouter from '../renderWithRouter';
+// import renderWithRouter from '../renderWithRouter';
 import AppProvider from '../provider/AppProvider';
 
 describe('Testando o componente <Header/>', () => {
@@ -22,36 +22,5 @@ describe('Testando o componente <Header/>', () => {
     expect(nameRadio).toBeVisible();
     expect(firstLetterRadio).toBeVisible();
     expect(searchButton).toBeVisible();
-  });
-
-  it('Redirecionamento para profile', () => {
-    const { history } = renderWithRouter(
-      <AppProvider>
-        <Header />
-      </AppProvider>,
-    );
-
-    const profileIcon = screen.getByTestId('profile-top-btn');
-    userEvent.click(profileIcon);
-
-    expect(history.location.pathname).toBe('/profile');
-    expect(profileIcon).toBeVisible();
-  });
-
-  it('Testa se ao clicar no botao a barra de procura aparece e se clicar novamente desaparece', () => {
-    render(
-      <AppProvider>
-        <Header />
-      </AppProvider>,
-    );
-
-    const searchButton = screen.getByTestId('search-top-btn');
-    userEvent.click(searchButton);
-
-    const inputSearch = screen.getByTestId('search-input');
-    expect(inputSearch).toBeVisible();
-
-    userEvent.click(searchButton);
-    expect(inputSearch).not.toBeVisible();
   });
 });
