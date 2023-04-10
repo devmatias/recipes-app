a3ece174eba6334eeaf47a5f813b22614cc602be
Thiago <xfatalgames@gmail.com>
2023-04-10 11:18:41 -0300

    Requisito 50 ao 56 com exceção do 55 e testes

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,7 +1,22 @@
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
+import FavoriteRecipeCard from '../components/FavoriteRecipeCard';
 
 function FavoriteRecipes() {
+  let favorites = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+  const [arrFavorites, setFavorites] = useState(favorites);
+  const filterAll = () => {
+    setFavorites(favorites);
+  };
+
+  const handleFavorite = (id) => {
+    favorites = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    const newFavorite = favorites.filter((recipe) => recipe.id !== id);
+    setFavorites([...newFavorite]);
+    localStorage.setItem('favoriteRecipes', JSON.stringify(newFavorite));
+  };
+
   const history = useHistory();
 
   const redirectPage = (path) => {
@@ -19,6 +34,40 @@ function FavoriteRecipes() {
           data-testid="profile-top-btn"
         />
       </button>
+      <button
+        onClick={ () => filterAll() }
+        data-testid="filter-by-all-btn"
+      >
+        All
+      </button>
+      <button
+        data-testid="filter-by-meal-btn"
+        onClick={ () => filterAll() }
+      >
+        Meals
+      </button>
+      <button
+        data-testid="filter-by-drink-btn"
+        onClick={ () => filterAll() }
+      >
+        Drinks
+      </button>
+      { (arrFavorites && arrFavorites.length > 0)
+        && arrFavorites.map((recipe, index) => (
+          <FavoriteRecipeCard
+            key={ index }
+            index={ index }
+            image={ recipe.image }
+            name={ recipe.name }
+            categoria={ `${recipe.nationality} - ${recipe.category} -
+            ${recipe.alcoholicOrNot}` }
+            date={ recipe.doneDate }
+            tags={ recipe.tags }
+            type={ recipe.type }
+            id={ recipe.id }
+            handleFavorite={ handleFavorite }
+          />
+        ))}
     </header>
   );
 }
