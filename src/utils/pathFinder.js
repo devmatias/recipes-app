1c84e67cbd4c535f7a8fd80eb8aa3391d987cd0a
devmatias <devmatiascff@gmail.com>
2023-04-12 16:11:55 -0300

    Requisito 37 completo

--- src/utils/pathFinder.js
+++ src/utils/pathFinder.js
@@ -1,5 +1,7 @@
 import { MealsContext, DrinksContext } from '../context/Context';
 import {
+  DETAILS_DRINKS,
+  DETAILS_MEALS,
   DRINKS_BRIEF_URL,
   MEALS_BRIEF_URL,
 } from './constants';
@@ -15,5 +17,12 @@ export function pathURLCategoryFinder(location) {
   let url = '';
   if (location.pathname.includes('/meals')) url = MEALS_BRIEF_URL;
   if (location.pathname.includes('/drinks')) url = DRINKS_BRIEF_URL;
+  return url;
+}
+
+export function pathURLRecipeFinder(location) {
+  let url = '';
+  if (location.pathname.includes('/meals')) url = DETAILS_MEALS;
+  if (location.pathname.includes('/drinks')) url = DETAILS_DRINKS;
   return url;
 }
